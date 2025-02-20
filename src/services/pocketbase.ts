38bf385e43c4259046f5b94b2e56bb61c29ad86c
Harilee <45534309+harilee1325@users.noreply.github.com>
2025-02-20 23:26:03 +0530

    updsted ngro

--- src/services/pocketbase.ts
+++ src/services/pocketbase.ts
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase';
 
-const pb = new PocketBase('https://bcc6-2401-4900-1f27-b9a3-54e1-4b1a-dc8c-3ae1.ngrok-free.app/'); // Change this if your PocketBase server is hosted
+const pb = new PocketBase('https://9310-2401-4900-1f26-70c4-74c7-93af-db1c-1458.ngrok-free.app/'); // Change this if your PocketBase server is hosted
 
 export default pb;
 
